9a6d189c7a600f338b18a170ee839e4494c81c33
Srilatha <msrilatha859@gmail.com>
2026-02-13 13:30:39 +0530

    updated portfolio

--- src/components/projectcard.jsx
+++ src/components/projectcard.jsx
@@ -1,8 +1,13 @@
-function ProjectCard({ title, description, badges, liveLink, githubLink }) {
+function ProjectCard({ image, title, description, badges, liveLink, githubLink }) {
   return (
     <div className="col-md-4">
-      <div className="card h-100 shadow-sm border-0 rounded-4">
+      <div className="card h-100 shadow-lg border-0 rounded-4">
+        
         <div className="card-body p-4 d-flex flex-column">
+          <div className="p-1">
+           <img src={image} className="img-fluid rounded mb-3"
+      style={{ height: "200px", objectFit: "cover", width: "100%" }} />
+           </div>
           <h5 className="card-title fw-semibold">{title}</h5>
 
           <p className="card-text flex-grow-1">
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,46 +4,57 @@ import ProjectCard from './ProjectCard';
 function Projects() {
   const projects = [
     {
-      title: "Hospital Management Website",
-      description:
-        "Responsive healthcare website with doctor profiles, blogs, and secure admin management.",
-      badges: [
-        { label: "React", className: "bg-primary" },
-        { label: "WordPress", className: "bg-secondary" },
-        { label: "MySQL", className: "bg-info text-dark" },
-      ],
-      liveLink: "#",
-      githubLink: "#",
-    },
-    {
-      title: "E-Commerce Website",
-      description:
-        "Online store built using Shopify and WordPress with Razorpay payment integration.",
-      badges: [
-        { label: "Shopify", className: "bg-success" },
-        { label: "WooCommerce", className: "bg-primary" },
-        { label: "Razorpay", className: "bg-warning text-dark" },
-      ],
-      liveLink: "#",
-      githubLink: "#",
-    },
-    {
-      title: "Digital Marketing Website",
-      description:
-        "SEO-friendly website with CRM integration and lead management features.",
-      badges: [
-        { label: "HTML", className: "bg-dark" },
-        { label: "CSS", className: "bg-dark" },
-        { label: "JavaScript", className: "bg-dark" },
-        { label: "Zoho CRM", className: "bg-info text-dark" },
-      ],
-      liveLink: "#",
-      githubLink: "#",
-    },
-  ];
+  title: "Netflix Clone (TMDB API)",
+  description:
+    "Responsive Netflix-inspired web app built with React and TMDB API. Features movie categories, dynamic banners, and real-time data fetching.",
+  image: "/netflix-clone.png", 
+  badges: [
+    { label: "React", className: "bg-primary" },
+    { label: "TMDB API", className: "bg-danger" },
+    { label: "Vite", className: "bg-warning text-dark" },
+  ],
+  liveLink: "https://sri-netfliix-clone.netlify.app/", 
+  githubLink: "https://github.com/msrilatha859-python/netflix-clone",
+},
+  {
+    title: "Interactive React Event App",
+    description:
+      "Dynamic React application featuring randomized button positioning and conditional modal rendering.",
+    image: "/fun-image1.png",
+    badges: [
+      { label: "React", className: "bg-primary" },
+      { label: "Tailwind CSS", className: "bg-info text-dark" },
+    ],
+    liveLink: "https://funny-form.netlify.app/",
+    githubLink: "https://github.com/msrilatha859-python/trending-form",
+  },
+  {
+    title: "Food Recipe API App",
+    description:
+      "React app that fetches food data from a public REST API with search functionality.",
+    image: "/api-food1.png",
+    badges: [
+      { label: "React", className: "bg-primary" },
+      { label: "REST API", className: "bg-success" },
+    ],
+    liveLink: "https://sri-basic-api.netlify.app/",
+    githubLink: "https://github.com/msrilatha859-python/basicapi",
+  },
+  {
+    title: "Python File Organizer Tool",
+    description:
+      "Python automation script that organizes files based on extensions.",
+    image: "/file-org.png",
+    badges: [
+      { label: "Python", className: "bg-warning text-dark" },
+      { label: "Automation", className: "bg-success" },
+    ],
+    githubLink: "https://github.com/msrilatha859-python/File-Organizer",
+  },
+];
 
   return (
-    <section className="py-5 bg-light" id="projects">
+    <section className="py-3 bg-light" id="projects">
       <div className="container">
         <h2 className="text-center fw-bold mb-5 text-primary">
           Projects
